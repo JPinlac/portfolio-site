6dd8862d10ff6fdf62eb67c40aa42f6349def686
Jonathan Pinlac <jonppinlac@gmail.com>
2016-01-19 15:38:58 -0500

    Shrinking navbar success

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-$('.navbar-start').delay(3000).queue(function(next){
-  $(this).addClass('navbar-normal');
-  $(this).removeClass('navbar-start');
-  $(this).offset();
+// shrink navbar back to auto. JQuery solution necessary because of height transition limitations in CSS
+var curHeight = $('#navbar').height();
+var autoHeight = $('#navbar').css('height', 'auto').height();
+console.log(autoHeight)
+$('#navbar').delay(3000).height(curHeight).animate({height: autoHeight}, 1000, function(){
+  $(this).removeAttr('style');
+  $(this).css('height', 'auto')
 });
