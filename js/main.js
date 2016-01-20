a1a42fe9bbbb33e9d5523a35f8abaafa056ba4c1
Jonathan Pinlac <jonppinlac@gmail.com>
2016-01-20 15:35:23 -0500

    More tweaking polishing intro section

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-// shrink navbar back to auto.
-// JQuery solution necessary because of transition property limitations in CSS with height auto
-var curHeight = $('#navbar').height();
-var autoHeight = $('#navbar').css('height', 'auto').height();
-
+// after intro changes regular values
 $('#navbar').delay(3000).queue(function(next){
   $(this).addClass('normal');
   $('#type-helper').addClass('normal');
   $('#title').addClass('show');
   $(this).removeAttr('style');
-  if ($(window).width() / parseFloat($('body').css('font-size')) <31.5){
+  if ($(window).width() / parseFloat($('body').css('font-size')) <= 32){
     $('nav ul').addClass('show');
   }
   next();
 });
 
+// shrink navbar back to auto.
+// JQuery solution necessary because of transition property limitations in CSS with height auto
+var curHeight = $('#navbar').height();
+var autoHeight = $('#navbar').css('height', 'auto').height();
 $('#navbar').height(curHeight).animate({height: autoHeight}, 1000, function(){
   $(this).removeAttr('style');
-  if ($(window).width() / parseFloat($('body').css('font-size')) >=31.5){
+  if ($(window).width() / parseFloat($('body').css('font-size')) > 32){
     $(this).css('align-items', 'flex-end');
     console.log('first')
   }
@@ -24,9 +24,10 @@ $('#navbar').height(curHeight).animate({height: autoHeight}, 1000, function(){
   $('nav ul').addClass('show');
 });
 
+// fix for full screen navbar intro breaking normal responsive design
 $(window).on('resize', function(){
   console.log($(this).width() / parseFloat($('body').css('font-size')))
-  if($(this).width() / parseFloat($('body').css('font-size')) <31.5){
+  if($(this).width() / parseFloat($('body').css('font-size')) <= 32){
     $('#navbar').css('align-items', 'center');
   }
   else {
