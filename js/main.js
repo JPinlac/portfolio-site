87a3dbf9f765d8faf0f2aa6609a122315d5bc3c9
Jonathan Pinlac <jonppinlac@gmail.com>
2016-01-20 09:26:55 -0500

    Messing with css transitions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-// shrink navbar back to auto. JQuery solution necessary because of height transition limitations in CSS
+// shrink navbar back to auto.
+// JQuery solution necessary because of transition property limitations in CSS with height auto
 var curHeight = $('#navbar').height();
 var autoHeight = $('#navbar').css('height', 'auto').height();
-console.log(autoHeight)
 $('#navbar').delay(3000).height(curHeight).animate({height: autoHeight}, 1000, function(){
   $(this).removeAttr('style');
-  $(this).css('height', 'auto')
+  $(this).css('height', 'auto');
+});
+$('#title, .navbar ul').delay(3000).queue(function(next){
+  $(this).addClass('show');
+  next();
 });
