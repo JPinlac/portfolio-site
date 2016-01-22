292ffdbaaea4ed9911378bde72088a6e9e594469
Jonathan Pinlac <jonppinlac@gmail.com>
2016-01-22 10:03:04 -0500

    new sticky, and cached navbar selector

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-// after intro changes regular values
-$('#navbar').delay(3000).queue(function(next){
+var navbar = $('#navbar')
+// after intro, changes to regular values
+navbar.delay(3000).queue(function(next){
   $(this).addClass('normal');
   $('#type-helper').addClass('normal');
   $('#title').addClass('show');
@@ -10,15 +11,14 @@ $('#navbar').delay(3000).queue(function(next){
   next();
 });
 
-// shrink navbar back to auto.
+// animated shrink navbar back to height: auto.
 // JQuery solution necessary because of transition property limitations in CSS with height auto
-var curHeight = $('#navbar').height();
-var autoHeight = $('#navbar').css('height', 'auto').height();
-$('#navbar').height(curHeight).animate({height: autoHeight}, 1000, function(){
+var curHeight = navbar.height();
+var autoHeight = navbar.css('height', 'auto').height();
+navbar.height(curHeight).animate({height: autoHeight}, 1000, function(){
   $(this).removeAttr('style');
   if ($(window).width() / parseFloat($('body').css('font-size')) > 32){
     $(this).css('align-items', 'flex-end');
-    console.log('first')
   }
   $(this).css('height', 'auto');
   $('nav ul').addClass('show');
@@ -26,12 +26,20 @@ $('#navbar').height(curHeight).animate({height: autoHeight}, 1000, function(){
 
 // fix for full screen navbar intro breaking normal responsive design
 $(window).on('resize', function(){
-  console.log($(this).width() / parseFloat($('body').css('font-size')))
   if($(this).width() / parseFloat($('body').css('font-size')) <= 32){
-    $('#navbar').css('align-items', 'center');
+    navbar.css('align-items', 'center');
   }
   else {
-    $('#navbar').css('align-items', 'flex-end');
-    console.log("second")
+    navbar.css('align-items', 'flex-end');
+  }
+});
+
+// sticky navbar
+$(window).scroll(function(){
+  if($(this).scrollTop() > 0) {
+    navbar.addClass('sticky');
+
+  } else {
+    navbar.removeClass('sticky');
   }
 });
