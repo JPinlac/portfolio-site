173aeae2b5d78f371d7aae3717d6825b6852301f
Jonathan Pinlac <jonppinlac@gmail.com>
2016-02-01 14:33:01 -0500

    navbar fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-var navbar = $('#navbar')
+var $navbar = $('#navbar')
 // after intro, changes to regular values
-navbar.delay(3000).queue(function(next){
+$navbar.delay(3000).queue(function(next){
   $(this).addClass('normal');
   $('#type-helper').addClass('normal');
   $('#title').addClass('show');
   $(this).removeAttr('style');
-  if ($('#navbar').css('flex-flow') == 'column wrap'){
+  if ($navbar.css('flex-flow') == 'column wrap'){
     $('nav ul').addClass('show');
   }
   next();
@@ -13,11 +13,11 @@ navbar.delay(3000).queue(function(next){
 
 // animated shrink navbar back to height: auto.
 // JQuery solution necessary because of transition property limitations in CSS with height auto
-var curHeight = navbar.height();
-var autoHeight = navbar.css('height', 'auto').height();
-navbar.height(curHeight).animate({height: autoHeight}, 1000, function(){
+var curHeight = $navbar.height();
+var autoHeight = $navbar.css('height', 'auto').height();
+$navbar.height(curHeight).animate({height: autoHeight}, 1000, function(){
   $(this).removeAttr('style');
-  if ($('#navbar').css('flex-flow') == 'row wrap'){
+  if ($navbar.css('flex-flow') == 'row wrap'){
     $(this).css('align-items', 'flex-end');
   }
   $(this).css('height', 'auto');
@@ -26,20 +26,27 @@ navbar.height(curHeight).animate({height: autoHeight}, 1000, function(){
 
 // fix for full screen navbar intro breaking normal responsive design
 $(window).on('resize', function(){
-  if ($('#navbar').css('flex-flow') == 'column wrap'){
-    navbar.css('align-items', 'center');
+  if ($navbar.css('flex-flow') == 'column wrap'){
+    $navbar.css('align-items', 'center');
   }
   else {
-    navbar.css('align-items', 'flex-end');
+    $navbar.css('align-items', 'flex-end');
   }
 });
 
+var $intro = $('intro')
 // sticky navbar
 $(window).scroll(function(){
   if($(this).scrollTop() > 0) {
-    navbar.addClass('sticky');
-
+    if($navbar.css('flex-flow') == 'row wrap'){
+      $intro.css('margin-top', $navbar.height());
+    }
+    $navbar.addClass('sticky');
   } else {
-    navbar.removeClass('sticky');
+    if($navbar.css('flex-flow') == 'column wrap'){
+      $intro.css('margin-top', 0);
+    }
+    $navbar.removeClass('sticky');
   }
 });
+
